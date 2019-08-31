a13eea51ff0d44f3d5580bfc9a938ed566e3836f
Mohib <mohib.0008@gmail.com>
2019-08-30 21:24:30 -0400

    nothing new just pulling

--- index.js
+++ index.js
@@ -15,35 +15,4 @@ var newsFeed = [
         username: "Mohibullah",
         timeline: "Javascript is so cool ! i wanna learn more about it"
     }
-<<<<<<< HEAD
-    return whatHappens;
-}
-
-var list = ["car", "Moterbig", "bysycle", "robatic", "Track"];
-
-var user = {
-    name: "Mohib",
-    lastName: "Arsala",
-    age: "33",
-    hobby: "coding",
-    isMarried: "false",
-    shout: function() {
-        console.log("fire on hime!!");
-    }
-};
-
-var list = [
-    {
-        userName: "Mohib",
-        password: "lllm"
-    },
-
-    {
-        userName: "Arsala",
-        password: "mmml",
-        action: "stright"
-    }
-]
-=======
 ];
->>>>>>> bbc233e3755a50e9e808a91e2f1de80a909b7d57
